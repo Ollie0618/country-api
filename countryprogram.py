896f4b084632a2ca687a53bf48eb7aada4f3aaca
Ollie0618 <oliverheinzt@gmail.com>
2025-04-23 14:12:34 +0200

    Code (Update 2)

--- countryprogram.py
+++ countryprogram.py
@@ -1,22 +1,25 @@
 import requests
 
-def find_befolkning_og_flag():
-    country = input("Indtast et land: ")
+def search_api(country):
     url = f"https://restcountries.com/v3.1/name/{country}"
     response = requests.get(url)
     
     if response.status_code != 200:
-        print(f"Fejl: Kunne ikke finde data for '{country}'.")
         return
 
     data = response.json()
     if not data:
-        print("Ingen data fundet.")
         return
 
     land_info = data[0]
-    befolkning = land_info.get("population", "Ukendt")
+    return land_info
 
-    print(f"Befolkning i {country}: {befolkning}")
 
-find_befolkning_og_flag()
+    
+country = input("Indtast et land: ")
+result = search_api(country)
+if result == None:
+    print("Ingen data fundet.")
+
+befolkning = result.get("population", "Ukendt")
+print(f"Befolkning i {country}: {befolkning}")
